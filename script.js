3eeccfc5a3b3676b1e70810bda490916891f6c81
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-02-11 15:55:22 +0100

    clicking bird 1 toggles first speech bubble

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const bubbles = document.querySelectorAll(".bubble");
 bubbles.forEach(bubble => {
   bubble.style.visibility = "hidden";  
+});
+
+const birds = document.querySelectorAll("div.bird-with-text");
+console.table(birds);
+birds.forEach(bird => {
+  bird.addEventListener('click', toggleVisibility)
 })
 
+function toggleVisibility() {
+  if (bubbles[0].style.visibility === "hidden") {
+    bubbles[0].style.visibility = "visible";
+  } else {
+    bubbles[0].style.visibility = "hidden";
+  }
+}

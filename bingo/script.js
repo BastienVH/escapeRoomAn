9a40b5aee7c03ed48ab970214a54a1b5b9f943c4
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-02-12 10:08:03 +0100

    changed variable iterator to number

---
+++ bingo/script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector("div.container");
+
+// Generate 5x5 grid
+drawGrid();
+
+function drawGrid() {
+  container.style.cssText += `grid-template-columns:repeat(5, 1fr);grid-template-rows:repeat(5, 1fr)`;
+  for (let number = 1; number <= 25; i++) {
+    cell = document.createElement("div");
+    cell.classList.add("cell");
+    // make each cell background black
+    cell.style.backgroundColor = "black";
+    cell.style.color = "white";
+    // fill grid with numbers 1 - 25
+    cell.textContent = number;
+    container.appendChild(cell);
+  }
+}
+
+// on cell click: make background blue by adding active class
+// if 5 specific cells have class active: promt "You win!"
